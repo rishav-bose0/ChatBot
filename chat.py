574c21a1d1543100f260630b09b7d7e7797892f7
rishav bose <rishav.bose1996@gmail.com>
2024-10-08 18:27:13 +0530

    add chatbot retrieval and pdf extract features

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 
 class GptLLM:
     def __init__(self):
-        self.api_key = "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"
+        self.api_key = "sk-g"
         self.llm = ChatOpenAI(model="gpt-4o-mini", api_key=self.api_key)
 
     def get_llm(self):
